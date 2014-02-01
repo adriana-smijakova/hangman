97cb3a0a46fdaa4134567a6bd2d85f60075a54ad
Adriana <adriana.smijakova@gmail.com>
2014-02-01 15:34:01 +0100

    Create basic table structure

---
+++ app/views/words/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @word, :id, :content, :category_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangman::Application.config.secret_key_base = '28efeb0f6f5ed9cb07acf001d5c0466dc6f2bf3f2936b339ce2870964f49d1cd494255da05ef47c9043f904145dcc38557941c26b4b424eda7ef1f9e48f179b7'
